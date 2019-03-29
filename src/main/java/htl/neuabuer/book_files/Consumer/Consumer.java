5a06ba3f7deb7e38439b3bd34493bddf0821ab9c
neukoc16 <ko.neubauer@gmail.com>
2019-03-29 12:19:23 +0100

    Further Implementation of Classes

--- src/main/java/htl/neuabuer/book_files/Consumer/Consumer.java
+++ src/main/java/htl/neuabuer/book_files/Consumer/Consumer.java
@@ -1,9 +1,22 @@
 package htl.neuabuer.book_files.Consumer;
 
+import htl.neuabuer.book_files.BL.Book;
+import htl.neuabuer.book_files.Queue.MyQueue;
+
 public class Consumer implements Runnable {
+
+    private MyQueue<Book> books;
+
+    public Consumer(MyQueue<Book> books) {
+        this.books = books;
+    }
 
     @Override
     public void run() {
-
+        while (true) {
+            synchronized (books) {
+                
+            }
+        }
     }
 }
--- src/main/java/htl/neuabuer/book_files/Producer/Producer.java
+++ src/main/java/htl/neuabuer/book_files/Producer/Producer.java
@@ -1,9 +1,22 @@
 package htl.neuabuer.book_files.Producer;
 
+import htl.neuabuer.book_files.BL.Book;
+import htl.neuabuer.book_files.Queue.MyQueue;
+
 public class Producer implements Runnable {
+
+    private MyQueue<Book> books;
+
+    public Producer(MyQueue<Book> books) {
+        this.books = books;
+    }
 
     @Override
     public void run() {
+        while (true) {
+            synchronized (books) {
 
+            }
+        }
     }
 }
