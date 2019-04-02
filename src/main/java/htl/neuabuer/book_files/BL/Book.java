fcdce3af5b8ce5b827ef83ce4817174abe10ae8e
neukoc16 <neukoc16@edv-htl.at>
2019-04-02 16:57:13 +0200

    Furthermore implemented Book

--- src/main/java/htl/neuabuer/book_files/BL/Book.java
+++ src/main/java/htl/neuabuer/book_files/BL/Book.java
@@ -1,11 +1,16 @@
 package htl.neuabuer.book_files.BL;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 
 public class Book {
 
     private final String inputfilename;
-    private final String text;
+    private String text;
 
     public Book(String inputfilename, String text) {
         this.inputfilename = inputfilename;
@@ -13,6 +18,28 @@ public class Book {
     }
 
     public HashMap<String, Integer> countWords() {
-        return null;
+        String words[] = text.split(" ");
+        HashMap<String, Integer> countWords = new HashMap<>();
+
+        for (String word : words) {
+            if (countWords.containsKey(word)) {
+                countWords.put(word, countWords.get(word) + 1);
+            } else {
+                countWords.put(word, 1);
+            }
+        }
+        for (String string : countWords.keySet()) {
+            if (countWords.get(string) == 1) {
+                countWords.remove(string);
+            }
+        }
+        return countWords;
+    }
+
+    public void load() throws FileNotFoundException, IOException {
+        BufferedReader br = new BufferedReader(new FileReader(new File(inputfilename)));
+        while (br.ready()) {
+            text += br.readLine();
+        }
     }
 }
